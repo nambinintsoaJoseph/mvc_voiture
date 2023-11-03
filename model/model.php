315b95697ce221dee44c4f8a992e95db59d86961
Nambinintsoa Joseph <raznambinintsoa3@gmail.com>
2023-11-03 15:47:48 +0100

    Implementation insertion

---
+++ model/model.php
@@ -0,0 +1,29 @@
+<?php 
+    require_once('../controler/connexion_base.php');
+    class Voiture {
+        private $_matricule; 
+        private $_couleur; 
+        private $_puissance; 
+        private $_type; 
+
+        public function __construct($matricule ,$couleur, $puissance, $type) {
+            $this->_matricule = $matricule;
+            $this->_couleur = $couleur; 
+            $this->_puissance = $puissance; 
+            $this->_type = $type; 
+        }
+
+        // Implémentation des méthodes : 
+        public function inserer_dans_la_base() {
+            $requette = $bdd->prepare('CALL ajouterVoiture(?, ?, ?, ?)');
+            $requette->execute(
+                array(
+                    'matricule' => $this->_matricule,
+                    'couleur' => $this->_couleur, 
+                    'puissance' => $this->_puissance,
+                    'type' => $this->_type
+                )
+            );
+        }
+    }
+?>
