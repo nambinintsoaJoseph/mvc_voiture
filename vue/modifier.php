02c379f7fc0bdac37ef6bcee8a07aee3cb5fb25f
Nambinintsoa Joseph <raznambinintsoa3@gmail.com>
2023-11-06 00:52:16 +0100

    Re-lecture terminé

--- vue/modifier.php
+++ vue/modifier.php
@@ -4,7 +4,7 @@
     $_SESSION['ancien_matricule'] = $_GET['matricule']; 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
