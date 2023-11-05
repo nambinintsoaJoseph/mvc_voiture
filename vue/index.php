0c830caa567790c0c9a9e573a0c1682c88d5768d
Nambinintsoa Joseph <raznambinintsoa3@gmail.com>
2023-11-06 00:21:43 +0100

    Organisation du code

--- vue/index.php
+++ vue/index.php
@@ -5,47 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-    <title>MVC - Voiture </title>
+    <title>MVC - Voiture</title>
 </head>
 <body>
     <div>
-        <a href="ajouter.php" class="add">Ajouter une voiture</a>
+        <a href="ajouter.php" class="btn">Ajouter une voiture</a>
     </div>
 
     <h1>Liste des voitures</h1>
 
     <?php require('../controler/connexion_base.php'); ?>
 
+    <table>
+        <tr>
+            <th>Matricule</th>
+            <th>Couleur</th>
+            <th>Puissance</th>
+            <th>Type</th>
+            <th colspan="2">Action</th>
+        </tr>
+
     <?php 
         $requette = $bdd->query('SELECT * FROM voiture ORDER BY id DESC');
 
-        while($donnee = $requette->fetch()) {
+        while($donnee = $requette->fetch()) 
+        {
     ?>
-    <div class="conteneur-flexible flex-direction-column conteneur-list-voiture">
-        <div>
-            <p>Matricule : <?php echo $donnee['matricule']; ?></p>
-            <input type="hidden" value="<?php echo $donnee['matricule']?>">
-        </div>
-
-        <div>
-            <p>Couleur : <?php echo $donnee['couleur']; ?> </p>
-        </div>
-
-        <div>
-            <p>Puissance : <?php echo $donnee['puissance']; ?> </p>
-        </div>
-        
-        <div>
-            <p>Type : <?php echo $donnee['type']; ?> </p>
-        </div>
-
-        <div class="conteneur-flexible">
-            <a href="../vue/modifier.php?<?php echo 'matricule=' . $donnee['matricule'] . '&couleur=' . $donnee['couleur'] . '&puissance=' . $donnee['puissance'] . '&type=' . $donnee['type']; ?>" class="flex-grow modifier">Modifier</a>
-            <a href="../controler/suppression.php?<?php echo 'matricule=' . $donnee['matricule']; ?>" class="flex-grow supprimer">Supprimer</a>
-        </div>
-    </div>
-    <?php 
+                <tr>
+                    <td><?php echo $donnee['matricule']; ?></td>
+                    <td><?php echo $donnee['couleur']; ?></td>
+                    <td><?php echo $donnee['puissance']; ?></td>
+                    <td><?php echo $donnee['type']; ?></td>
+                    <td><a href="../vue/modifier.php?<?php echo 'matricule=' . $donnee['matricule'] . '&couleur=' . $donnee['couleur'] . '&puissance=' . $donnee['puissance'] . '&type=' . $donnee['type']; ?>" class="flex-grow modifier btn">Modifier</a></td>
+                    <td><a href="../controler/suppression.php?<?php echo 'matricule=' . $donnee['matricule']; ?>" class="flex-grow supprimer btn">Supprimer</a></td>
+                </tr>
+            <?php 
+                
         }
-    ?>
+        ?>
+    </table>
 </body>
 </html>
